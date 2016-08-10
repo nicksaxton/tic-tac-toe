b46b4a324fcb0e08a211261bd5cce4045a6dd8c0
Nick Saxton <nick.saxton@gmail.com>
2016-08-09 22:59:14 -0400

    First working version

--- js/script.js
+++ js/script.js
@@ -13,6 +13,37 @@ $(document).ready(function() {
     var spot8 = $("#spot8");
     var spot9 = $("#spot9");
 
+    var reset = function() {
+        spot1.removeClass("o");
+        spot1.removeClass("x");
+        spot2.removeClass("o");
+        spot2.removeClass("x");
+        spot3.removeClass("o");
+        spot3.removeClass("x");
+        spot4.removeClass("o");
+        spot4.removeClass("x");
+        spot5.removeClass("o");
+        spot5.removeClass("x");
+        spot6.removeClass("o");
+        spot6.removeClass("x");
+        spot7.removeClass("o");
+        spot7.removeClass("x");
+        spot8.removeClass("o");
+        spot8.removeClass("x");
+        spot9.removeClass("o");
+        spot9.removeClass("x");
+        spot1.html("+");
+        spot2.html("+");
+        spot3.html("+");
+        spot4.html("+");
+        spot5.html("+");
+        spot6.html("+");
+        spot7.html("+");
+        spot8.html("+");
+        spot9.html("+");
+        turns = 0;
+    }
+
     $("#board li").on("click", function() {
         if(($(this).hasClass("o")) || ($(this).hasClass("x")))
         {
@@ -34,6 +65,46 @@ $(document).ready(function() {
             }
 
             turns++;
+
+            setTimeout(function() {
+                /* Check for win or draw */
+            if((spot1.hasClass("x") && spot2.hasClass("x") && spot3.hasClass("x")) ||
+               (spot1.hasClass("x") && spot4.hasClass("x") && spot7.hasClass("x")) ||
+               (spot1.hasClass("x") && spot5.hasClass("x") && spot9.hasClass("x")) ||
+               (spot2.hasClass("x") && spot5.hasClass("x") && spot8.hasClass("x")) ||
+               (spot3.hasClass("x") && spot5.hasClass("x") && spot7.hasClass("x")) ||
+               (spot3.hasClass("x") && spot6.hasClass("x") && spot7.hasClass("x")) ||
+               (spot4.hasClass("x") && spot5.hasClass("x") && spot6.hasCLass("x")) ||
+               (spot7.hasClass("x") && spot8.hasClass("x") && spot9.hasClass("x")))
+            {
+                alert("X wins!");
+                reset();
+            }
+            else if((spot1.hasClass("o") && spot2.hasClass("o") && spot3.hasClass("o")) ||
+                    (spot1.hasClass("o") && spot4.hasClass("o") && spot7.hasClass("o")) ||
+                    (spot1.hasClass("o") && spot5.hasClass("o") && spot9.hasClass("o")) ||
+                    (spot2.hasClass("o") && spot5.hasClass("o") && spot8.hasClass("o")) ||
+                    (spot3.hasClass("o") && spot5.hasClass("o") && spot7.hasClass("o")) ||
+                    (spot3.hasClass("o") && spot6.hasClass("o") && spot7.hasClass("o")) ||
+                    (spot4.hasClass("o") && spot5.hasClass("o") && spot6.hasClass("o")) ||
+                    (spot7.hasClass("o") && spot8.hasClass("o") && spot9.hasClass("o")))
+            {
+                alert("O wins!");
+                reset();
+            }
+            else
+            {
+                if(turns > 8)
+                {
+                    alert("Tie!");
+                    reset();
+                }
+            }
+            }, 100);
+
+            
         }
     });
+
+    $("#reset").on("click", reset);
 });
